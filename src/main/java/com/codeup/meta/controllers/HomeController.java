5e315fb4d672e0a435f1258daaa188ff2029b5f3
Stout13 <stout.stephen13@gmail.com>
2022-06-29 14:38:07 -0500

    COntrollers

---
+++ src/main/java/com/codeup/meta/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.codeup.meta.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    @ResponseBody
+    public String main() {
+        return "This is the landing page!";
+    }
+}
