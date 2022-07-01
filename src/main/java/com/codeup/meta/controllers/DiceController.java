8d67b2f88582b1e88a11268fe6ff86c551620170
Stout13 <stout.stephen13@gmail.com>
2022-06-30 19:59:19 -0500

    Meta dice-roll

--- src/main/java/com/codeup/meta/controllers/DiceController.java
+++ src/main/java/com/codeup/meta/controllers/DiceController.java
@@ -1,22 +1,35 @@
 package com.codeup.meta.controllers;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class DiceController {
 
-    @GetMapping("/roll-dice")
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello";
+    }
+
+    @GetMapping(path = "/roll-dice")
     public String showDice() {
         return "roll-dice";
     }
 
 
-
     @PostMapping(path = "/roll-dice")
     public String rollDice(@RequestParam(name = "guess") Double guess, Model model) {
-        boolean roll = guess == Math.floor(Math.random() * 100 * 5 + 1);
+        String roll;
+        if (guess == Math.floor(Math.random() * 100) * 5 + 1) {
+            roll = "Nice" + guess + " was correct! How do you do it?!";
+        } else {
+            roll = "Dude" + guess + "? Do you even Vegas bruh?!";
+        }
+
         model.addAttribute("roll", roll);
         return "roll-dice";
     }
--- src/main/java/com/codeup/meta/controllers/MathController.java
+++ src/main/java/com/codeup/meta/controllers/MathController.java
@@ -1,44 +1,44 @@
-package com.codeup.meta.controllers;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-@Controller
-public class MathController {
-//    @GetMapping("/")
+//package com.codeup.meta.controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.*;
+//
+//@Controller
+//public class MathController {
+////    @GetMapping("/")
+////    @ResponseBody
+////    public String main() {
+////        return "This is the landing page!";
+////    }
+//    @GetMapping(path = "/add/{number1}/and/{number2}")
 //    @ResponseBody
-//    public String main() {
-//        return "This is the landing page!";
+//    public String addNumbers(@PathVariable int number1, int number2) {
+//
+//        int sum = number1 + number2;
+//        return (number1 + " plus " + number2 + " is " + sum + "!");
 //    }
-    @GetMapping(path = "/add/{number1}/and/{number2}")
-    @ResponseBody
-    public String addNumbers(@PathVariable int number1, int number2) {
-
-        int sum = number1 + number2;
-        return (number1 + " plus " + number2 + " is " + sum + "!");
-    }
-
-    @GetMapping(path = "/subtract/{number1}/from/{number2}")
-    @ResponseBody
-    public String subtractNumbers(@PathVariable int number1, int number2) {
-
-        int sum = number1 + number2;
-        return (number1 + " minus " + number2 + " is " + sum + "!");
-    }
-
-    @GetMapping(path = "/subtract/{number1}/from/{number2}")
-    @ResponseBody
-    public String multiplyNumbers(@PathVariable int number1, int number2) {
-
-        int product = number1 * number2;
-        return (number1 + " times " + number2 + " is " + product + "!");
-    }
-
-    @GetMapping(path = "/divide/{number1}/by/{number2}")
-    @ResponseBody
-    public String divideNumbers(@PathVariable int number1, int number2) {
-
-        int quotient = number1/number2;
-        return (number1 + " plus " + number2 + " is " + quotient + "!");
-    }
-}
+//
+//    @GetMapping(path = "/subtract/{number1}/from/{number2}")
+//    @ResponseBody
+//    public String subtractNumbers(@PathVariable int number1, int number2) {
+//
+//        int sum = number1 + number2;
+//        return (number1 + " minus " + number2 + " is " + sum + "!");
+//    }
+//
+//    @GetMapping(path = "/subtract/{number1}/from/{number2}")
+//    @ResponseBody
+//    public String multiplyNumbers(@PathVariable int number1, int number2) {
+//
+//        int product = number1 * number2;
+//        return (number1 + " times " + number2 + " is " + product + "!");
+//    }
+//
+//    @GetMapping(path = "/divide/{number1}/by/{number2}")
+//    @ResponseBody
+//    public String divideNumbers(@PathVariable int number1, int number2) {
+//
+//        int quotient = number1/number2;
+//        return (number1 + " plus " + number2 + " is " + quotient + "!");
+//    }
+//}
