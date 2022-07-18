af53fac3f3a692290783eeb7d5f66f677364c02c
Stout13 <stout.stephen13@gmail.com>
2022-07-18 09:14:34 -0500

    Services

--- src/main/java/com/codeup/meta/controllers/AdController.java
+++ src/main/java/com/codeup/meta/controllers/AdController.java
@@ -1,43 +1,53 @@
-//package com.codeup.meta.controllers;
-//
-//import com.codeup.meta.models.Ad;
-//import com.codeup.meta.models.AdRepository;
-//import com.codeup.meta.models.PostRepository;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//
-//@Controller
-//
-//public class AdController {
-//
-//        // These two next steps are often called dependency injection, where we create a Repository instance and initialize it in the controller class constructor.
-//        private com.codeup.meta.models.AdRepository adsRepository;
-//
-//        AdController(AdRepository adsRepository){
-//            this.adsRepository = adsRepository;
-//        }
-////
-////        public AdController(com.codeup.meta.models.AdRepository adRepository) {
-////            this.adsRepository = adRepository;
-////        }
-//
-//        @GetMapping("/ads")
-//        public String index(Model model) {
-//            model.addAttribute("ads", adsRepository.findAll());
-//            return "ads/index";
-//        }
-//
+package com.codeup.meta.controllers;
 
-//        @PostMapping("ads/create")
-//            public String postAd(@RequestParam(name="title") String title, @RequestParam(name="description") String description){
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.codeup.meta.models.AdRepository;
+import com.codeup.meta.services.EmailService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AdController {
+
+
+
+
+
+        private final EmailService emailService;
+
+        public AdController(EmailService emailService) {
+            this.emailService = emailService;
+        }
+
+        // These two next steps are often called dependency injection, where we create a Repository instance and initialize it in the controller class constructor.
+        private com.codeup.meta.models.AdRepository adsRepository;
+
+        AdController(EmailService emailService, AdRepository adsRepository){
+            this.emailService = emailService;
+            this.adsRepository = adsRepository;
+        }
 //
-//            newAd.setTitle(title);
-//            newAd.setDescription(description);
-//
-//            adsRepository.save(newAd);
-//                return redirect:/ads;
-//            }
-//}
+//        public AdController(com.codeup.meta.models.AdRepository adRepository) {
+//            this.adsRepository = adRepository;
+//        }
+
+        @GetMapping("/ads")
+        public String index(Model model) {
+            model.addAttribute("ads", adsRepository.findAll());
+            return "ads/index";
+        }
+
+
+        @PostMapping("ads/create")
+            public String postAd(@RequestParam(name="title") String title, @RequestParam(name="description") String description){
+
+            newAd.setTitle(title);
+            newAd.setDescription(description);
+
+            adsRepository.save(newAd);
+                return redirect:/ads;
+            }
+}
